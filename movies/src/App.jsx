163e244d9e8f03e4b57bc9a2c940024a1d220c53
mugira <gichurujoseph61@gmail.com>
2023-08-17 17:25:37 +0300

    functional

--- movies/src/App.jsx
+++ movies/src/App.jsx
@@ -5,54 +5,33 @@ import Movie from "./movie";
 
 function App() {
   const [movieList, setMovieList] = useState([]);
-  const getaMovie = () => {
-    for (let i = 1; i <= 10; i++) {
-      let id = Math.ceil(Math.random() * 9999999);
-      const url = `http://www.omdbapi.com/?i=+tt${id}&apikey=272e6847`;
-      fetch(url)
-        .then((res) => res.json())
-        .then((data) => {
-          let movie = movieList.push(data);
-          setMovieList([...movie]);
-        });
-    }
-    console.log(movieList.length);
-  };
-  //getaMovie();
-  const getMovie = async () => {
-    let id = Math.ceil(Math.random() * 9999999);
-    const url = `http://www.omdbapi.com/?i=+tt${id}&apikey=272e6847`;
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error("Data coud not be fetched!");
-    } else {
-      return response.json();
-    }
-  };
   useEffect(() => {
-    for (let i = 1; i <= 10; i++) {
-      getMovie()
-        .then((data) => {
-          let movie = movieList.push(data);
-          setMovieList([...movie]);
-        })
-        .catch((e) => {
-          console.log(e.message);
-        });
-    }
+    const get = async () => {
+      for (let i = 1; i <= 10; i++) {
+        let id = Math.ceil(Math.random() * 9999999);
+        //const url = ()=>
+        await fetch(`http://www.omdbapi.com/?i=+tt${id}&apikey=272e6847`)
+          .then((res) => res.json())
+          .then((data) => {
+            movieList.push(data);
+            setMovieList([...movieList]);
+          });
+      }
+    };
+    get();
   }, []);
+
   return (
     <>
       <Nav changeMovie={(lock) => setMovieList(lock)} />
       <br />
-      <main style={{ textAlign: "center" }} onLoad={getMovie}>
-        <button onClick={getMovie} className="btn btn-primary">
-          GET RANDOM MOVIE
-        </button>
+      <main style={{ textAlign: "center" }}>
         <div className="row row-cols-1 row-cols-md-2 g-4">
-          {movieList.map((movie) => {
-            return <Movie movie={movie} />;
-          })}
+          {movieList &&
+            movieList.length > 0 &&
+            movieList.map((movie) => {
+              return <Movie movie={movie} />;
+            })}
         </div>
       </main>
     </>
